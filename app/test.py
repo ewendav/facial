50403d5c3642fb5704cc06b1e531223bb07aa576
John Doe <johndoe@example.com>
2024-01-23 15:46:29 +0100

    azef

--- app/test.py
+++ app/test.py
@@ -139,7 +139,7 @@ def recognize_faces(trained_model_file, frame):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Set up the face detector
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    face_cascade = cv2.CascadeClassifier('../hash.xml')
 
     # Detect faces in the frame
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
