db71fab408a7424bbda357cded42dca796f0de99
John Doe <johndoe@example.com>
2024-01-23 13:47:18 +0100

    g

--- app/test.py
+++ app/test.py
@@ -6,9 +6,7 @@ picam2.preview_configuration.main.format = "RGB888"
 picam2.preview_configuration.align()
 picam2.configure("preview")
 picam2.start()
-while True:
-    im= picam2.capture_array()
-    cv2.imshow("Camera", im)
-    if cv2.waitKey(1)==ord('q'):
-        break
+im= picam2.capture_array()
+cv2.imshow("Camera", im)
+if cv2.waitKey(1)==ord('q'):
 cv2.destroyAllWindows()
