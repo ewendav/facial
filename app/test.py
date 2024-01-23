2866e3a8a5fbe6febc1c4652e9aa2e90c6680f95
John Doe <johndoe@example.com>
2024-01-23 16:11:51 +0100

    fz

--- app/test.py
+++ app/test.py
@@ -45,6 +45,7 @@ import cv2
 import numpy as np
 from picamera2 import Picamera2
 
+
 def photoEntrainement(output_xml_file):
     (images, labels, label_id) = ([], [], 0)
 
@@ -97,79 +98,97 @@ def photoEntrainement(output_xml_file):
 
 
 
-infirmiereNom = input("nom de l'infirmière")
-
-output_xml_file = infirmiereNom + '_model.xml'
-photoEntrainement(output_xml_file)
 
 
-# import cv2
-# from picamera2 import Picamera2
-# import numpy as np
 
-# def recognize_faces(trained_model_file, frame):
-#     # Load the trained face recognition model
-#     model = cv2.face.LBPHFaceRecognizer_create()
-#     model.read(trained_model_file)
 
-#     # Convert the frame to grayscale
-#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+def recognize_faces(trained_model_file, frame):
+    # Load the trained face recognition model
+    model = cv2.face.LBPHFaceRecognizer_create()
+    model.read(trained_model_file)
 
-#     # Set up the face detector
-#     face_cascade = cv2.CascadeClassifier('../hash.xml')
+    # Convert the frame to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-#     # Detect faces in the frame
-#     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+    # Set up the face detector
+    face_cascade = cv2.CascadeClassifier('../hash.xml')
 
-#     for (x, y, w, h) in faces:
-#         # Crop the face region
-#         face_region = gray[y:y+h, x:x+w]
+    # Detect faces in the frame
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
 
-#         # Resize the face region to a standard size
-#         face_region_resized = cv2.resize(face_region, (112, 92))
+    for (x, y, w, h) in faces:
+        # Crop the face region
+        face_region = gray[y:y+h, x:x+w]
 
-#         # Recognize the face using the trained model
-#         label, confidence = model.predict(face_region_resized)
+        # Resize the face region to a standard size
+        face_region_resized = cv2.resize(face_region, (112, 92))
 
-#         # Draw a rectangle around the face
-#         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        # Recognize the face using the trained model
+        label, confidence = model.predict(face_region_resized)
 
-#         # Display the recognized name and confidence level
-#         recognized_name = f"Person {label} ({confidence:.2f})"
-#         cv2.putText(frame, recognized_name, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+        # Draw a rectangle around the face
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-#     return frame
+        # Display the recognized name and confidence level
+        recognized_name = f"Person {label} ({confidence:.2f})"
+        cv2.putText(frame, recognized_name, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
-# def main():
-#     trained_model_file = 'nino_model.xml'
+    return frame
 
-#     picam2 = Picamera2()
-#     picam2.preview_configuration.main.size = (1280, 720)
-#     picam2.preview_configuration.main.format = "RGB888"
-#     picam2.preview_configuration.align()
-#     picam2.configure("preview")
-#     picam2.start()
+def main():
+   
 
-#     try:
-#         while True:
-#             # Capture the camera image
-#             im = picam2.capture_array()
+if __name__ == "__main__":
+    main()
 
-#             # Perform face recognition on the captured frame
-#             im_with_recognition = recognize_faces(trained_model_file, im)
 
-#             # Display the combined image with both raw camera and face recognition
-#             cv2.imshow("Camera with Face Recognition", im_with_recognition)
+print("1 = prendre photo")
+print("2 = tester model")
+choix = input('choisissez : ')
 
-#             # Break the loop when 'q' is pressed
-#             if cv2.waitKey(1) == ord('q'):
-#                 break
+while True :
+    match choix :
 
-#     finally:
-#         # Release resources
-#         cv2.destroyAllWindows()
-#         picam2.stop()
-#         picam2.close()
+    case 1:
+        infirmiereNom = input("donne nom de l'infirmière : ")
+        output_xml_file = infirmiereNom + '_model.xml'
+        photoEntrainement(output_xml_file)
+        break
 
-# if __name__ == "__main__":
-#     main()
+    case 2:
+        infirmiereNom = input("donne nom de l'infirmière : ")
+        trained_model_file = infirmiereNom + '_model.xml'
+
+            picam2 = Picamera2()
+            picam2.preview_configuration.main.size = (1280, 720)
+            picam2.preview_configuration.main.format = "RGB888"
+            picam2.preview_configuration.align()
+            picam2.configure("preview")
+            picam2.start()
+
+            try:
+                while True:
+                    # Capture the camera image
+                    im = picam2.capture_array()
+
+                    # Perform face recognition on the captured frame
+                    im_with_recognition = recognize_faces(trained_model_file, im)
+
+                    # Display the combined image with both raw camera and face recognition
+                    cv2.imshow("Camera with Face Recognition", im_with_recognition)
+
+                    # Break the loop when 'q' is pressed
+                    if cv2.waitKey(1) == ord('q'):
+                        break
+
+            finally:
+                # Release resources
+                cv2.destroyAllWindows()
+                picam2.stop()
+                picam2.close()
+                break
+
+    case _:
+        print('pas compris') 
+
+
