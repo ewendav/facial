6d16c3fe0296a99f69ec804bcc940181d365b684
John Doe <johndoe@example.com>
2024-01-23 14:07:03 +0100

    zef

--- app/test.py
+++ app/test.py
@@ -1,11 +1,11 @@
 import cv2
 from picamera2 import Picamera2
-picam2 = Picamera2()
-picam2.preview_configuration.main.size = (1280,720)
-picam2.preview_configuration.main.format = "RGB888"
-picam2.preview_configuration.align()
-picam2.configure("preview")
-picam2.start()
+# picam2 = Picamera2()
+# picam2.preview_configuration.main.size = (1280,720)
+# picam2.preview_configuration.main.format = "RGB888"
+# picam2.preview_configuration.align()
+# picam2.configure("preview")
+# picam2.start()
 
 process_this_frame = True
 
