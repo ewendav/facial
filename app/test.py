a1d9ffab86c3c94d9d1647ca9423e25797effd04
John Doe <johndoe@example.com>
2024-01-23 15:02:32 +0100

    ef

--- app/test.py
+++ app/test.py
@@ -3,29 +3,29 @@ import cv2
 import numpy as np
 
 # Load the pre-trained Haarcascades face detector
-face_cascade = cv2.CascadeClassifier('../hash.xml' + 'haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 # Initialize the Picamera2
 picamera = Picamera2()
 
 # Set preview configuration
 picamera.preview_configuration.main.size = (640, 480)
-picamera.preview_configuration.main.format = "bgr"  # Ensure BGR format
+picamera.preview_configuration.main.format = "rgb"  # Change to "rgb" or "bgr"
 picamera.preview_configuration.align()
 picamera.configure("preview")
 picamera.start()
 
 while True:
     # Capture a single image
-    frame = picamera.capture(format='bgr')
+    frame = picamera.capture(format='rgb')  # Change to "rgb" or "bgr" as needed
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # Convert to gray
+    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)  # Convert to gray
     faces = face_cascade.detectMultiScale(gray,
                                           scaleFactor=1.2,
                                           minNeighbors=5,
                                           minSize=(80, 80))
 
-    # Draw a rectangle around the faces 
+    # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
