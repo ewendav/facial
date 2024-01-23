57c8c58175b1b353231cbc288adf4578e23e8872
John Doe <johndoe@example.com>
2024-01-23 14:30:06 +0100

    fze

--- app/test.py
+++ app/test.py
@@ -2,13 +2,14 @@ from picamera2 import Picamera2
 import cv2
 import numpy as np
 import time
+import face_recognition
 
 # Initialize the Picamera2
 picamera = Picamera2()
 
 # Set preview configuration
 picamera.preview_configuration.main.size = (640, 480)
-picamera.preview_configuration.main.format = "rgb"
+picamera.preview_configuration.main.format = "bgr"  # Ensure BGR format
 picamera.preview_configuration.align()
 picamera.configure("preview")
 picamera.start()
@@ -16,27 +17,34 @@ picamera.start()
 # Create an array to store the image
 image_array = np.empty((480, 640, 3), dtype=np.uint8)
 
-# Load the pre-trained Haarcascades face detector
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-
 try:
     # Capture frames indefinitely
     while True:
         # Capture a single image into the array
-        picamera.capture(image_array, 'bgr')  # Change 'rgb' to 'bgr'
+        picamera.capture(image_array, 'bgr')
 
-        # Convert the image to grayscale for face detection
-        gray_image = cv2.cvtColor(image_array, cv2.COLOR_BGR2GRAY)
+        # Resize frame of video to 1/4 size for faster face detection processing
+        small_frame = cv2.resize(image_array, (0, 0), fx=0.25, fy=0.25)
 
-        # Detect faces in the grayscale image
-        faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.3, minNeighbors=5)
+        # Convert the image from BGR to RGB
+        rgb_small_frame = small_frame[:, :, ::-1]
+
+        # Find all the faces in the current frame of video
+        face_locations = face_recognition.face_locations(rgb_small_frame)
 
         # Draw rectangles around the faces
-        for (x, y, w, h) in faces:
-            cv2.rectangle(image_array, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        for (top, right, bottom, left) in face_locations:
+            # Scale the face locations back up since the frame was resized
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+
+            # Draw a rectangle around the face
+            cv2.rectangle(image_array, (left, top), (right, bottom), (0, 255, 0), 2)
 
         # Display the image with face rectangles
-        cv2.imshow('Face Detection', image_array)
+        cv2.imshow('Facial Detection', image_array)
 
         # Break the loop if 'q' key is pressed
         if cv2.waitKey(1) & 0xFF == ord('q'):
