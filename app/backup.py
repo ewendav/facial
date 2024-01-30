c1cd33a769418694b6c994be7212f9d71f635384
John Doe <johndoe@example.com>
2024-01-30 14:23:14 +0100

    sd

---
+++ app/backup.py
@@ -0,0 +1,303 @@
+# import cv2
+# from picamera2 import Picamera2
+
+# # Load the pre-trained Haar Cascade classifier for face detection
+# face_cascade = cv2.CascadeClassifier('../hash.xml')
+
+# camera = Picamera2()
+# camera.preview_configuration.main.size = (1280, 720)
+# camera.preview_configuration.main.format = "RGB888"
+# camera.preview_configuration.align()
+# camera.configure("preview")
+# camera.start()
+
+# try:
+#     while True:
+#         # Capture the camera image
+#         im = camera.capture_array()
+
+#         # Convert the image to grayscale for face detection
+#         gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+
+#         # Perform face detection
+#         faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))
+
+#         # Draw rectangles around the detected faces
+#         for (x, y, w, h) in faces:
+#             cv2.rectangle(im, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+#         # Display the combined image with both raw camera and face detection
+#         cv2.imshow("Camera with Face Detection", im)
+
+#         # Break the loop when 'q' is pressed
+#         if cv2.waitKey(1) == ord('q'):
+#             break
+
+# finally:
+#     # Release resources
+#     cv2.destroyAllWindows()
+#     camera.stop()
+#     camera.close()
+
+
+
+import cv2
+import numpy as np
+from picamera2 import Picamera2, Preview
+import os
+import cv2
+import sys
+import time
+
+# sys.path.append('dependencies/picameraFolder/picamera')  
+
+
+def prendsPhotos():
+    size = 4
+    fn_haar = '../hash.xml'
+    fn_dir = 'Photos'
+    count_max = 30
+
+    try:
+        print("Identifiant de l'infirmière")
+        fn_name = input()
+        if len(fn_name) == 0:
+            print("Vous devez fournir un identifiant valide !")
+            sys.exit(0)
+        print("Nom du dossier des Photos, enter si 'Photos'")
+        fn_dir1 = input()
+        if len(fn_dir1) > 0:
+            fn_dir = fn_dir1
+        print("Nombre de photos, par défaut 30 enter si ok")
+        count_max1 = input()
+        if len(count_max1) > 0:
+            count_max = int(str(count_max1))
+    except:
+        print("Erreur de saisie !")
+        sys.exit(0)
+
+    path = os.path.join(fn_dir, fn_name)
+    if not os.path.isdir(path):
+        os.mkdir(path)
+
+    (im_width, im_height) = (112, 92)
+    haar_cascade = cv2.CascadeClassifier(fn_haar)
+
+    # Create the PiCamera2 object
+    camera = Picamera2()
+    camera_config = camera.create_still_configuration(main={"size": (1920, 1080)}, lores={"size": (640, 480)}, display="lores")
+    camera.configure(camera_config)
+
+    pin = sorted([int(n[:n.find('.')]) for n in os.listdir(path) if n[0] != '.'] + [0])[-1] + 1
+
+    print("\n\033[94mLe programme va enregistrer " + str(count_max) + " photos. \
+    Veuillez bouger la tête pour prendre des photos de face différenciées.\033[0m\n")
+
+
+    count = 0
+    pause = 0
+    camera.start()
+    time.sleep(2)
+
+
+    while count < 30:
+        frame = None
+        try:
+            frame = camera.capture_array("main")
+        except KeyboardInterrupt:
+            break
+        except Exception as e:
+            print(f"Error capturing frame: {e}")
+            continue
+
+
+        height, width, channels = frame.shape
+        frame = cv2.flip(frame, 1, 0)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        mini = cv2.resize(gray, (int(gray.shape[1] / size), int(gray.shape[0] / size)))
+
+        # Detect faces
+        faces = haar_cascade.detectMultiScale(mini)
+
+        # We only consider the largest face
+        faces = sorted(faces, key=lambda x: x[3])
+        if faces:
+            face_i = faces[0]
+            (x, y, w, h) = [v * size for v in face_i]
+
+            face = gray[y:y + h, x:x + w]
+            face_resize = cv2.resize(face, (im_width, im_height))
+
+            # Draw a rectangle and write the identifier
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+            cv2.putText(frame, fn_name, (x - 10, y - 10), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0))
+
+            # Do not consider faces that are too small
+            if (w * 6 < width or h * 6 < height):
+                print("Face trop petite")
+            else:
+                # To create diversity, only save every fifth detected image
+                if (pause == 0):
+                    print("Sauvegarde de la photo " + str(count + 1) + "/" + str(count_max))
+
+                    # Save image file
+                    cv2.imwrite('%s/%s.png' % (path, pin), face_resize)
+
+                    pin += 1
+                    count += 1
+                    pause = 1
+
+        if (pause > 0):
+            pause = (pause + 1) % 5
+
+        cv2.imshow('OpenCV', frame)
+        key = cv2.waitKey(1) & 0xFF
+
+        # If the 'Esc' key is pressed, break from the loop
+        if key == 27:
+            break
+
+    # Release the camera resources
+    camera.close()
+    cv2.destroyAllWindows()
+
+
+
+def entrainementPhoto():
+     
+    size = 4
+    fn_haar = '../hash.xml'
+    fn_dir = 'Photos'
+
+    (images, lables, names, id) = ([], [], {}, 0)
+    for (subdirs, dirs, files) in os.walk(fn_dir):
+        for subdir in dirs:
+            names[id] = subdir
+            subjectpath = os.path.join(fn_dir, subdir)
+            for filename in os.listdir(subjectpath):
+                f_name, f_extension = os.path.splitext(filename)
+                if(f_extension.lower() not in ['.png','.jpg','.jpeg','.gif','.pgm']):
+                    continue
+                path = subjectpath + '/' + filename
+                lable = id
+                images.append(cv2.imread(path, 0))
+                lables.append(int(lable))
+            id += 1
+    (im_width, im_height) = (112, 92)
+    print(images)
+    # Create a Numpy array from the two lists above
+    (images, lables) = [np.array(lis) for lis in [images, lables]]
+
+    model = cv2.face.LBPHFaceRecognizer_create()
+    model.train(images, lables)
+
+    model.save("models/" + 'master' + "_model.xml")
+    print("Training complete. Model overrided as " + 'master' + "_model.xml")
+
+
+
+def ReconnaissanceFacial():
+    size = 4
+    fn_haar = '../hash.xml'
+    folder_path = 'Photos'
+    names = {}
+
+    subdirectories = [d for d in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, d))]
+    for label, subdir in enumerate(subdirectories):
+        names[label] = subdir
+    
+
+    model = cv2.face.LBPHFaceRecognizer_create()
+    model.read('models/' + 'master' + '_model.xml')  
+
+    (im_width, im_height) = (112, 92)
+    haar_cascade = cv2.CascadeClassifier(fn_haar)
+
+    camera = Picamera2()
+    camera_config = camera.create_still_configuration(main={"size": (1920, 1080)}, lores={"size": (640, 480)}, display="lores")
+    camera.configure(camera_config)
+    camera.start()
+    time.sleep(2)
+
+    pasReconnu = True
+    retour = False
+    
+    while pasReconnu:
+        frame = None
+
+        try:
+            frame = camera.capture_array("main")
+        except KeyboardInterrupt:
+            break
+        except Exception as e:
+            print(f"Error capturing frame: {e}")
+            continue
+
+        height, width, channels = frame.shape
+        frame = cv2.flip(frame, 1, 0)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        mini = cv2.resize(gray, (int(gray.shape[1] / size), int(gray.shape[0] / size)))
+
+        # # Detect faces 
+        faces = haar_cascade.detectMultiScale(mini)
+
+        for i in range(len(faces)):
+            face_i = faces[i]
+
+            # Coordinates of face after scaling back by `size`
+            (x, y, w, h) = [v * size for v in face_i]
+            face = gray[y:y + h, x:x + w]
+            face_resize = cv2.resize(face, (112, 92))  
+
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+            prediction = model.predict(face_resize)
+
+            # Try to recognize the face
+            if prediction[1] < 90:
+                cv2.putText(frame,'%s - %.0f' % (names[prediction[0]],prediction[1]),(x-10, y-10), cv2.FONT_HERSHEY_PLAIN,1,(0, 255, 0))
+                # if names[prediction[0]] == name:
+                #     retour = True
+                #     pasReconnu = False
+                #     print(f"Face recognized: {name}")
+
+        cv2.imshow('OpenCV', frame)
+
+        key = cv2.waitKey(1)
+        if key != -1:
+            key = key & 0xFF
+        
+        if key == 27:
+            break
+
+
+
+    camera.close()
+    cv2.destroyAllWindows()
+
+    return retour
+
+
+
+
+
+
+while True:
+    print("1 = prendre photo")
+    print('2 = entrainer le model sur les photos')
+    print("3 = tester model")
+
+    choix = int(input("Choisissez une option (1, 2, 3.): "))
+
+    if choix == 1:
+        prendsPhotos()
+        print('photos prises')
+
+    elif choix == 2:
+        print('entrainement du model sur toutes les dossier de Photos ')
+        entrainementPhoto()
+
+    elif choix == 3:
+        ReconnaissanceFacial()
+
+    else:
+        print('Option non reconnue. Essayez à nouveau.')
