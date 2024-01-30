a1d30a571f864ae6d09d2b3e5e2735233fc59924
John Doe <johndoe@example.com>
2024-01-30 14:51:57 +0100

    sdf

--- app/test.py
+++ app/test.py
@@ -210,7 +210,7 @@ def ReconnaissanceFacial(name = ''):
             # Try to recognize the face
             if prediction[1] < 90:
                 cv2.putText(frame,'%s - %.0f' % (names[prediction[0]],prediction[1]),(x-10, y-10), cv2.FONT_HERSHEY_PLAIN,1,(0, 255, 0))
-                if name is not '':
+                if name != '':
                     if names[prediction[0]] == name:
                         retour = True
                         pasReconnu = False
