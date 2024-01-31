67caf1a5e64840407c14d1e38005673b3b41afdc
John Doe <johndoe@example.com>
2024-01-31 14:45:52 +0100

    ffxg

--- app/View.py
+++ app/View.py
@@ -41,7 +41,7 @@ class View:
             
     def microBit(self, result):
         if result:
-            microbit.sdisplay.show(microbit.Image.YES)
+            microbit.display.show(microbit.Image.YES)
             microbit.sleep(400)
 
         else:
